3864cd9db3659546f55386d15743682c24787398
Oscar Holm <oscar@numbery.se>
2021-12-25 10:04:44 +0100

    finished part1 courseinfo

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Header = (props) => {
-  return <h1>{props.course}</h1>;
+  return <h1>{props.courseName}</h1>;
 };
 
 const Part = ({ name, number }) => {
@@ -32,27 +32,29 @@ const Total = (props) => {
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const altParts = [
-    {
-      name: "Fundamentals of React",
-      number: 10,
-    },
-    {
-      name: "Using props to pass data",
-      number: 7,
-    },
-    {
-      name: "State of a component",
-      number: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        number: 10,
+      },
+      {
+        name: "Using props to pass data",
+        number: 7,
+      },
+      {
+        name: "State of a component",
+        number: 14,
+      },
+    ],
+  };
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={altParts} />
-      <Total parts={altParts} />
+      <Header courseName={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
